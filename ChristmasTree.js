bfe08f7c001fc4b2027be7773dff25af21ad7153
Iñaki Andrés <gancholillo@gmail.com>
2022-01-25 17:01:43 +0100

    Refactor in the script
    Removed all unnecessary logic
    Added more floors to the base

--- ChristmasTree.js
+++ ChristmasTree.js
@@ -4,34 +4,24 @@
     const heightTree = process.argv[2];
     const christmasFelicitation = process.argv[3];
     
-    const colors = ["\x1b[31m", "\x1b[32m", "\x1b[33m"];
-    
+    const COLORS = ["\x1b[31m", "\x1b[32m", "\x1b[33m"];
+
+        // Draw tree
         for (let rows=1; rows <= heightTree; rows+=2)
         {
             const lap = Math.floor(rows/christmasFelicitation.length);
-            let charToPrint = rows < christmasFelicitation.length ? 
+            const charToPrint = rows < christmasFelicitation.length ? 
                 christmasFelicitation.slice(0, rows)
                 : christmasFelicitation.repeat(lap)+christmasFelicitation.slice(0, rows-christmasFelicitation.length*(lap));
-            let tree ='';
-            const number_of_spaces = heightTree-rows/2;
-            for (let spaces=0; spaces <= number_of_spaces; spaces++)
-            {
-                tree += " ";
-            }
-            tree += charToPrint;
-            for (let spaces=1; spaces <= number_of_spaces; spaces++)
-            {
-                tree += " ";
-            }
             
-            console.log(colors[Math.floor(Math.random() * colors.length)], tree);
+            const number_of_spaces = heightTree-rows/2;
+
+            const tree = (" ".repeat(number_of_spaces) + charToPrint);
+            console.log(COLORS[Math.floor(Math.random() * COLORS.length)], tree);
         }
-        let base = '';
+
+        // Draw base
         const baseText = '||||||||||'
-        for (let spaces=1; spaces <= heightTree-baseText.length/2; spaces++)
-        {
-            base += " ";
-        }
-        base += baseText;
-        console.log('\x1b[36m%s\x1b[0m', base)
+        const base = " ".repeat(heightTree-baseText.length/2) + baseText + '\n';
+        console.log('\x1b[36m%s\x1b[0m', base.repeat(heightTree/10));
 })();
